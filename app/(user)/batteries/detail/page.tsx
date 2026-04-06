810bc73771974980e35093782ff81576ac556371
Faheem <faheemk793@gmail.com>
2026-04-06 14:03:54 +0530

    added brochures

--- app/(user)/batteries/detail/page.tsx
+++ app/(user)/batteries/detail/page.tsx
@@ -1,8 +1,18 @@
 "use client";
 import Hero from "../../sections/sub-apparel/hero";
 import Products from "../../sections/sub-apparel/products";
-import Brochure from "@/app/(user)/sections/brochure";
+import { Download } from "lucide-react";
+import Link from "next/link";
 import CTASection from "@/app/(user)/sections/cta";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 /* ===============================
    AUTOMOTIVE BATTERIES
 ================================ */
@@ -208,19 +218,112 @@ const page = () => {
       </div>
 
       <section className="mt-10 mb-20">
-        <Brochure
-          img={"/battery/details/brochure.png"}
-          title={
-            <h2 className="text-3xl mb-3 font-rubik">
-              <span className="text-primary">Smart Energy</span> Storage
-              Batteries for Everyday Performance
-            </h2>
-          }
-          desc={
-            "We offer advanced battery solutions that ensure consistent performance, long life, and dependable energy storage. Built to support critical operations, our batteries enable efficient power management across industries."
-          }
-          btn={"battery/details/Lucas Four Wheeler Battery Brochure.pdf"}
-        />
+        <div className=" bg-[#FCFAF5] py-16">
+          <div className="container">
+            <div className="grid grid-cols-1 items-center lg:grid-cols-2 gap-12">
+              <div className="h-[400px] bg-white border-gray-300 overflow-hidden rounded-xl border ">
+                <img
+                  src={"/battery/details/brochure.png"}
+                  alt=""
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <div>
+                <h2 className="text-3xl mb-3 font-rubik">
+                  <span className="text-primary">Smart Energy</span> Storage
+                  Batteries for Everyday Performance
+                </h2>
+                <p>
+                  We offer advanced battery solutions that ensure consistent
+                  performance, long life, and dependable energy storage. Built
+                  to support critical operations, our batteries enable efficient
+                  power management across industries.
+                </p>
+
+                <div className="lg:flex-row flex-col  flex lg:items-center gap-2 lg:gap-4 ">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <button className="px-5 w-fit hover:bg-primary/80 mt-5 flex items-center gap-2 py-2 bg-primary text-white">
+                        <Download />
+                        King Kong Brochures
+                      </button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="bg-white">
+                      {[
+                        {
+                          label: "King Kong E-Rickshaw Battery Brochure",
+                          link: "King Kong E-Rickshaw Battery Brochure",
+                        },
+                        {
+                          label: "King Kong EV Motorbikes & Scooters",
+                          link: "King Kong EV Motorbikes-and-Scooters.pdf",
+                        },
+                        {
+                          label: "King Kong Four Wheeler Battery Brochure",
+                          link: "King Kong Four Wheeler Battery Brochure.pdf",
+                        },
+                        {
+                          label:
+                            "King Kong Inverter Tall Tubular Battery Brochure",
+                          link: "King Kong Inverter Tall Tubular Battery Brochure.pdf",
+                        },
+                        {
+                          label:
+                            "King Kong Lithium Tall Tubular Battery Brochure",
+                          link: "King Kong Lithium Tall Tubular Battery Brochure.pdf",
+                        },
+                        {
+                          label: "King Kong Solar Tubular Battery Brochure",
+                          link: "King Kong Solar Tubular Battery Brochure.pdf",
+                        },
+                        {
+                          label: "King Kong Trucks & Busses Battery Brochure",
+                          link: "King Kong Trucks-and-Busses Battery Brochure.pdf",
+                        },
+                        {
+                          label: "King Kong Two Wheeler Battery Brochure",
+                          link: "King Kong Two Wheeler Battery Brochure.pdf",
+                        },
+                        {
+                          label:
+                            "King Kong Wall & Rack Mounted Lithium Battery Brochure",
+                          link: "King Kong Wall-and-Rack Mounted Lithium Battery Brochure.pdf",
+                        },
+                      ].map((item, idx) => (
+                        <DropdownMenuItem
+                          key={idx}
+                          className="hover:bg-primary/20"
+                        >
+                          <Link
+                            href={`/king-kong/${item.link}`}
+                            target="_blank"
+                          >
+                            <button className="flex   items-center text-gray-800 gap-2">
+                              <Download />
+                              {item.label}
+                            </button>
+                          </Link>
+                        </DropdownMenuItem>
+                      ))}
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+
+                  <Link
+                    href={
+                      "/battery/details/Lucas Four Wheeler Battery Brochure.pdf"
+                    }
+                    target="_blank"
+                  >
+                    <button className="px-5 mt-5 flex hover:bg-primary/80 items-center gap-2 py-2 bg-primary text-white">
+                      <Download />
+                      Lucas Brochure
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
 
       <CTASection />
